ccb28a651143d7d88e07fd747391a6147fb0920d
gleb-erokhin <gleb.erokhin@gmail.com>
2024-03-31 06:53:56 +0300

    task2

--- Duducalo/lesson 5/Task_2/main.js
+++ Duducalo/lesson 5/Task_2/main.js
@@ -4,66 +4,80 @@
 
 // Определите повторяющийся фрагмент кода и создайте для этого фрагмента отдельную функцию с параметрами (аргументами). Вызывайте функцию с параметрами в нужной части программы.
 
-document.write('<div class="container">')
-document.write('<h1 class="main-title">Приготовление пельменей</h1>')
+document.write('<div class="container">');
+document.write('<h1 class="main-title">Приготовление пельменей</h1>');
+document.write('<ul class="list">');
 
-let index = 0 // Счетчик этапов
+let index = 0; // Счетчик этапов
 
-document.write('<ul class="list">')
+function list(text, className = "") {
+  index++;
+  document.write(`<li class="list__item ${className}">`);
+  document.write(`<span class="list__index">${index}</span>`);
+  document.write(`<p class="list__text">${text}</p> `);
+  document.write(`</li>`);
+}
 
-// Этап 1
-index++
-let text1 = "Налейте в кастрюлю воду"
-let className1 = ""
-document.write(`<li class="list__item ${className1}">`)
-document.write(`<span class="list__index">${index}</span>`)
-document.write(`<p class="list__text">${text1}</p> `)
-document.write(`</li>`)
+list("Налейте в кастрюлю воду");
+list("Достаньте из холодильника пельмени");
+list("Забросьте пельмени в горячую воду. ОСТОРОЖНО, НЕ ОБОЖГИТЕСЬ!", "list__item_warning");
+list("Посолите и поперчите по вкусу");
+list("Ждите готовности");
+list("Приятного аппетита!", "list__item_warning");
 
-// Этап 2
-index++
-let text2 = "Достаньте из холодильника пельмени"
-let className2 = ""
-document.write(`<li class="list__item ${className2}">`)
-document.write(`<span class="list__index">${index}</span>`)
-document.write(`<p class="list__text">${text2}</p> `)
-document.write(`</li>`)
+// // Этап 1
+// index++;
+// let text1 = "Налейте в кастрюлю воду";
+// let className1 = "";
+// document.write(`<li class="list__item ${className1}">`);
+// document.write(`<span class="list__index">${index}</span>`);
+// document.write(`<p class="list__text">${text1}</p> `);
+// document.write(`</li>`);
 
-// Этап 3
-index++
-let text3 = "Забросьте пельмени в горячую воду. ОСТОРОЖНО, НЕ ОБОЖГИТЕСЬ!"
-let className3 = "list__item_warning"
-document.write(`<li class="list__item ${className3}">`)
-document.write(`<span class="list__index">${index}</span>`)
-document.write(`<p class="list__text">${text3}</p> `)
-document.write(`</li>`)
+// // Этап 2
+// index++;
+// let text2 = "Достаньте из холодильника пельмени";
+// let className2 = "";
+// document.write(`<li class="list__item ${className2}">`);
+// document.write(`<span class="list__index">${index}</span>`);
+// document.write(`<p class="list__text">${text2}</p> `);
+// document.write(`</li>`);
 
-// Этап 4
-index++
-let text4 = "Посолите и поперчите по вкусу"
-let className4 = ""
-document.write(`<li class="list__item ${className4}">`)
-document.write(`<span class="list__index">${index}</span>`)
-document.write(`<p class="list__text">${text4}</p> `)
-document.write(`</li>`)
+// // Этап 3
+// index++;
+// let text3 = "Забросьте пельмени в горячую воду. ОСТОРОЖНО, НЕ ОБОЖГИТЕСЬ!";
+// let className3 = "list__item_warning";
+// document.write(`<li class="list__item ${className3}">`);
+// document.write(`<span class="list__index">${index}</span>`);
+// document.write(`<p class="list__text">${text3}</p> `);
+// document.write(`</li>`);
 
-// Этап 5
-index++
-let text5 = "Ждите готовности"
-let className5 = ""
-document.write(`<li class="list__item ${className5}">`)
-document.write(`<span class="list__index">${index}</span>`)
-document.write(`<p class="list__text">${text5}</p> `)
-document.write(`</li>`)
+// // Этап 4
+// index++;
+// let text4 = "Посолите и поперчите по вкусу";
+// let className4 = "";
+// document.write(`<li class="list__item ${className4}">`);
+// document.write(`<span class="list__index">${index}</span>`);
+// document.write(`<p class="list__text">${text4}</p> `);
+// document.write(`</li>`);
 
-// Этап 6
-index++
-let text6 = "Приятного аппетита!"
-let className6 = "list__item_finish"
-document.write(`<li class="list__item ${className6}">`)
-document.write(`<span class="list__index">${index}</span>`)
-document.write(`<p class="list__text">${text6}</p> `)
-document.write(`</li>`)
+// // Этап 5
+// index++;
+// let text5 = "Ждите готовности";
+// let className5 = "";
+// document.write(`<li class="list__item ${className5}">`);
+// document.write(`<span class="list__index">${index}</span>`);
+// document.write(`<p class="list__text">${text5}</p> `);
+// document.write(`</li>`);
 
-document.write('</ul>')
-document.write('</div>')
+// // Этап 6
+// index++;
+// let text6 = "Приятного аппетита!";
+// let className6 = "list__item_finish";
+// document.write(`<li class="list__item ${className6}">`);
+// document.write(`<span class="list__index">${index}</span>`);
+// document.write(`<p class="list__text">${text6}</p> `);
+// document.write(`</li>`);
+
+document.write("</ul>");
+document.write("</div>");
