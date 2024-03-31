102fd7ec49035c652d894b9586fe104b2b318893
gleb-erokhin <gleb.erokhin@gmail.com>
2024-03-31 05:55:29 +0300

    инициализация репозитория, 4 задание

---
+++ Duducalo/lesson 4/Task_2/main.js
@@ -0,0 +1,48 @@
+// Исправьте код, так что бы в итоговой сумме покупки выводился правильный результат. 
+
+// Используйте инкремент для расчета порядкового номера.
+
+document.write('<h1 class="main-title">Список покупок</h1>')
+document.write('<ul class="product-list">')
+
+let index = 0 // Счетчик порядкового номера
+let totalPrice = 0 // Суммарная стоимость
+
+// Товар 1
+index = index + 1
+let product1 = prompt(`Введите название товара ${index}`) // Название товара
+let productPrice1 = Number(prompt(`Введите стоимость товара: "${product1}"`)) // Стоимость товара.
+console.log(typeof productPrice1);
+totalPrice = totalPrice + productPrice1
+
+document.write(`<li class="product-item">
+                  ${index}) <strong>${product1}</strong>,
+                  стоимоть: <strong>${productPrice1}</strong>
+                </li>`)
+
+// Товар 2
+index = index + 1
+let product2 = prompt(`Введите название товара ${index}`) // Название товара
+let productPrice2 = Number(prompt(`Введите стоимость товара: "${product2}"`)) // Стоимость товара.
+totalPrice = totalPrice + productPrice2
+
+document.write(`<li class="product-item">
+                  ${index}) <strong>${product2}</strong>,
+                  стоимоть: <strong>${productPrice2}</strong>
+                </li>`)
+
+// Товар 3
+index = index + 1
+let product3 = prompt(`Введите название товара ${index}`) // Название товара
+let productPrice3 = Number(prompt(`Введите стоимость товара: "${product3}"`)) // Стоимость товара.
+totalPrice = totalPrice + productPrice3
+
+document.write(`<li class="product-item">
+                  ${index}) <strong>${product3}</strong>,
+                  стоимоть: <strong>${productPrice3}</strong>
+                </li>`)
+
+document.write("</ul>")
+
+// Суммарная стоимость
+document.write(`<p class="total-price">Суммарная стоимость: <strong>${totalPrice}</strong></p>`)
