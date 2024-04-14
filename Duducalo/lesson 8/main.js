44012580bcd34fe24f3df1f2c3d1b8d097665604
gleb-erokhin <gleb.erokhin@gmail.com>
2024-04-14 18:44:24 +0300

    Создание общего шаблона, css и наполение страницы

---
+++ Duducalo/lesson 8/main.js
@@ -0,0 +1,10 @@
+let age = Number(prompt('enter the age'));
+
+document.write(`возраст пользователя ${age} БикЮ                                            `)
+
+if (age === 18) {
+    document.write(` пользователю ${age}`)
+} else {
+    document.write(` пользователю не 18`)
+
+}2
